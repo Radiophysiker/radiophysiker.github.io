95b875378ec687cd287a7f302aa288eefd5bada5
Radiophysiker <dmitrijbolshakov1988@mail.ru>
2017-11-21 02:51:26 +0300

    W3C

--- js/scrypt.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$("a").click(function () {
-        var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top;
-        $('body').animate({ scrollTop: destination }, 1100); 
-    });
-}); 
